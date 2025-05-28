c878b0428d90b78dd2bf26f9983c779689f8df4c
SJShep <sjshepp2@asu.edu>
2025-05-27 18:21:58 -0700

    initial work on processing timelapse acquisitions

--- src/opm_processing/imageprocessing/flatfield.py
+++ src/opm_processing/imageprocessing/flatfield.py
@@ -21,19 +21,28 @@ def no_op(*args, **kwargs):
     pass
 
 def estimate_illuminations(datastore, camera_offset,camera_conversion):
+    # flatfields shape: c, y, x
     flatfields = np.zeros((datastore.shape[2],datastore.shape[-2],datastore.shape[-1]),dtype=np.float32)
-    n_image_batches = 25
+    
+    # Define the number of z-images for flat field calculation
     if datastore.shape[-3] > 5000:
+        n_image_batches = 25
         n_rand_images = 5000
+    elif datastore.shape[-3]==1:
+        n_image_batches = 1 
+        n_rand_images = 1
     else:
+        n_image_batches = 25
         n_rand_images = datastore.shape[-3]
         n_rand_images -= n_rand_images % n_image_batches
     n_images_to_max = n_rand_images // n_image_batches
-    
+        
+    # Define the number of position samples for flat field calculation
     n_pos_samples = 15
     if datastore.shape[1] > n_pos_samples+5:
         flatfield_pos_iterator = list(np.random.choice(range(datastore.shape[1]//2-(n_pos_samples+5)//2,datastore.shape[1]//2+(n_pos_samples+5)//2), size=n_pos_samples, replace=False))
     else:
+        n_pos_samples = datastore.shape[1]
         flatfield_pos_iterator = range(datastore.shape[1])
     
     for chan_idx in range(datastore.shape[2]):
